1fdc31f73db6d7c215483b4edd960d868e5d4847
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2022-03-14 22:15:08 +0100

    Initial version

---
+++ precache-manifest.c8e74f1c6cb149ca1b25ed018b23af68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22efdc8d2f22d40112a9311d54e4fd7c",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "f0e155087925391d16be",
+    "url": "/css/chunk-vendors.9b2045b2.css"
+  },
+  {
+    "revision": "fa270f59488c7ca8d55d6c83f3147940",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acbb8e13b95b97d73d60",
+    "url": "/js/app.9d2504eb.js"
+  },
+  {
+    "revision": "f0e155087925391d16be",
+    "url": "/js/chunk-vendors.ad97e2cc.js"
+  },
+  {
+    "revision": "f4672e57c3d96c4340ba7615e453f418",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
